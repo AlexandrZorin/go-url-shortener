286ff7a3e63506d537e9c6565d4d36d848f54576
a.zorin <a.zorin@technex.ru>
2025-08-01 19:00:30 +0500

    sprint 1 increment 1 error checking

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,3 +1,48 @@
 package main
 
-func main() {}
+import (
+	"net/http"
+	"strings"
+)
+
+type URL struct {
+	urls map[string]string
+}
+
+var urlStorage = URL{
+	urls: make(map[string]string),
+}
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc(`/`, postHandler)
+	mux.HandleFunc(`/{id}`, getHandler)
+	err := http.ListenAndServe(`:8080`, nil)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func postHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Этот URL принимает только POST запросы", http.StatusBadRequest)
+		return
+	}
+	contentType := r.Header.Get("Content-Type")
+	if !strings.HasPrefix(contentType, "text/plain") {
+		http.Error(w, "Неверный Content-Type. Ожидается text/plain", http.StatusBadRequest)
+		return
+	}
+}
+
+func getHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		http.Error(w, "Этот URL принимает только GET запросы", http.StatusBadRequest)
+		return
+	}
+	id := strings.TrimPrefix(r.URL.Path, "/")
+	if id == "" {
+		http.Error(w, "Неверный запрос", http.StatusBadRequest)
+		return
+	}
+}
