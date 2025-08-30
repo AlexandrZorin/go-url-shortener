47b52a2375b7fbff3c6ae7545faef914874fda3d
Evolver <Evol-e@mail.ru>
2025-08-30 17:17:02 +0500

    sprint 2 increment 6 logger was created

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/AlexandrZorin/go-url-shortener/internal/config"
 	"github.com/AlexandrZorin/go-url-shortener/internal/handler"
+	"github.com/AlexandrZorin/go-url-shortener/internal/logger"
 	"github.com/gin-gonic/gin"
 )
 
@@ -18,6 +19,7 @@ func run() error {
 	cfg := config.CreateConfig()
 
 	r := gin.Default()
+	r.Use(logger.LoggerMiddleware())
 	handler.SetupRoutes(r, cfg)
 	r.Run(cfg.ServerAddress)
 	return nil
